2d21dacede4253acdf4809ef9e91ce785179cdd5
Henrik Jungberg <henkesnya@gmail.com>
2025-06-08 22:37:31 +0200

    sista fix?

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import logo from "../images/logo.png"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../images/Logo.png";
 
 const Nav = () => {
   return (
-    <nav  className='sidebar'>
-      <div >
-      <img src={logo} alt="" />
+    <nav className="sidebar">
+      <div>
+        <img src={logo} alt="" />
       </div>
       <NavLink to="/">Events</NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
