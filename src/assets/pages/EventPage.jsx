139e436a5c6837b18731cfa15a810e0bcf2ddbda
Henrik Jungberg <henkesnya@gmail.com>
2025-06-08 20:17:43 +0200

    Versaler

--- src/assets/pages/EventPage.jsx
+++ src/assets/pages/EventPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import EventList from "../components/EventList";
-import Nav from "../components/nav";
-import Header from "../components/header";
+import Nav from "../components/Nav";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const EventPage = () => {
